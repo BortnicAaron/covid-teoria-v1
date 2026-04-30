5cd8648d8d47ca36862cbea4841ac13eef8d2ca2
Aaron Bortinc <aaronbortinc@MacBook-Pro-de-Aaron.local>
2026-04-30 16:27:23 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // Vite + React. Servidor de dev con apertura automática del navegador.
 export default defineConfig({
-  base: '/covid-teoria/',
+  base: '/covid-teoria-v1/',
   plugins: [react()],
   server: {
     port: 5173,
